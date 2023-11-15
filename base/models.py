58cbfec4b407d9a9330b5c2d0c122cf627c412f5
prokelly <prokelly815@gmail.com>
2023-11-15 17:11:56 +0100

    rename media files upload directory

--- base/models.py
+++ base/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 class Files(models.Model):
-    file = models.FileField(upload_to="file")
+    file = models.FileField(upload_to="images")
 
 # Create your models here.
