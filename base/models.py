e885a59d67359e2558f81b066455090ce1230d36
prokelly <prokelly815@gmail.com>
2023-11-15 14:30:58 +0100

    added the django model for the files, added the url and a view for the project

--- base/models.py
+++ base/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+
+class Files(models.Model):
+    file = models.ImageField(upload_to="medai")
+
 # Create your models here.
--- base/views.py
+++ base/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Files
 
-# Create your views here.
+def index(request):
+    
+    if request.method == "POST":
+        files = request.FILES.getlist('files')
+        for file in files:
+            new_file = Files.objects.create(
+                file = file
+            )
+            new_file.save()
+            
+    context = {'new_file':new_file}
+    return render(request, 'base/index.html', context)
